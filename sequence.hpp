d68edf2315a7613e7234dbc7237659942637a9e9
Chao Zhang <chz069@ucsd.edu>
2020-10-03 01:39:44 -0700

    Add files via upload

--- sequence.hpp
+++ sequence.hpp
@@ -5,44 +5,31 @@
 using namespace std;
 
 inline score_t scorePos(const array<array<int, 4>, 3> cnt, const array<score_t, 4> pi){
-	count_t sum[3] = {0, 0, 0};
-	for (int x = 0; x < 3; x++){
-		for (int a = 0; a < 4; a++){
-			sum[x] += cnt[x][a];
-		}
-	}
-	/*
-	score_t res = 0;
-	for (int a = 0; a < 4; a++){
-		for (int b = 0; b < 4; b++){
-			if (b == a) continue;
-			res += (1 - pi[b]) * ( ((score_t) cnt[0][a]) * (cnt[0][a] - 1) * cnt[1][b] * cnt[2][b] + ((score_t) cnt[0][b]) * (cnt[0][b] - 1) * cnt[1][a] * cnt[2][a]
-			                     + ((score_t) cnt[1][a]) * (cnt[1][a] - 1) * cnt[2][b] * cnt[0][b] + ((score_t) cnt[1][b]) * (cnt[1][b] - 1) * cnt[2][a] * cnt[0][a]
-			                     + ((score_t) cnt[2][a]) * (cnt[2][a] - 1) * cnt[0][b] * cnt[1][b] + ((score_t) cnt[2][b]) * (cnt[2][b] - 1) * cnt[0][a] * cnt[1][a] )
-			       - pi[a] * (pi[a] + pi[b]) * ( ((score_t) cnt[0][b]) * (cnt[0][b] - 1) * sum[1] * sum[2] + ((score_t) sum[0]) * (sum[0] - 1) * cnt[1][b] * cnt[2][b]
-                                               + ((score_t) cnt[1][b]) * (cnt[1][b] - 1) * sum[2] * sum[0] + ((score_t) sum[1]) * (sum[1] - 1) * cnt[2][b] * cnt[0][b]
-                                               + ((score_t) cnt[2][b]) * (cnt[2][b] - 1) * sum[0] * sum[1] + ((score_t) sum[2]) * (sum[2] - 1) * cnt[0][b] * cnt[1][b] );
-		}
-	}
-	return res;
-	*/
-	score_t spi = 0;
-	score_t p0v0 = 0, p0v1 = 0, p0v2 = 0, p1v0 = 0, p1v1 = 0, p1v2 = 0, p2v0 = 0, p2v1 = 0, p2v2 = 0;
-	score_t q0v0 = 0, q0v1 = 0, q0v2 = 0, q1v0 = 0, q1v1 = 0, q1v2 = 0, q2v0 = 0, q2v1 = 0, q2v2 = 0;
-	for (int a = 0; a < 4; a++){
-		spi += pi[a] * pi[a];
-	}
-	for (int a = 0; a < 4; a++){
-		count_t cnt0 = cnt[0][a], cnt1 = cnt[1][a], cnt2 = cnt[2][a];
-		score_t p0 = cnt0 * (cnt0 - 1LL), p1 = cnt1 * (cnt1 - 1LL), p2 = cnt2 * (cnt2 - 1LL);
-		score_t q0 = cnt1 * cnt2, q1 = cnt2 * cnt0, q2 = cnt0 * cnt1;
-		score_t npi = 1 - pi[a], mpi = spi + pi[a] - 2 * pi[a] * pi[a];
-		p0v0 += p0; p0v1 += npi * p0; p0v2 += mpi * p0; q0v0 += q0; q0v1 += npi * q0; q0v2 += mpi * q0;
-		p1v0 += p1; p1v1 += npi * p1; p1v2 += mpi * p1; q1v0 += q1; q1v1 += npi * q1; q1v2 += mpi * q1;
-		p2v0 += p2; p2v1 += npi * p2; p2v2 += mpi * p2; q2v0 += q2; q2v1 += npi * q2; q2v2 += mpi * q2;
-	}
-	return p0v0 * q0v1 + p0v1 * q0v0 + p1v0 * q1v1 + p1v1 * q1v0 + p2v0 * q2v1 + p2v1 * q2v0
-		- (sum[1] * sum[2] * p0v2 + sum[0] * (sum[0] - 1) * q0v2 + sum[2] * sum[0] * p1v2 + sum[1] * (sum[1] - 1) * q1v2 + sum[0] * sum[1] * p2v2 + sum[2] * (sum[2] - 1) * q2v2);
+	//   AAGG*2*Y^2 + AAYY*2*G*R - YYAG*A*R - AAGY*2*R*Y
+	// + AAGG*2*Y^2 + GGYY*2*A*R - YYAG*G*R - GGAY*2*R*Y
+	//---------------------------------------------------
+	// + AAGG*4*Y^2              - YYAG*R^2
+	
+	const score_t A = pi[0], C = pi[1], G = pi[2], T = pi[3], R = A + G, Y = C + T;
+	const long long a0 = cnt[0][0], c0 = cnt[0][1], g0 = cnt[0][2], t0 = cnt[0][3], r0 = a0 + g0, y0 = c0 + t0;
+	const long long a1 = cnt[1][0], c1 = cnt[1][1], g1 = cnt[1][2], t1 = cnt[1][3], r1 = a1 + g1, y1 = c1 + t1;
+	const long long a2 = cnt[2][0], c2 = cnt[2][1], g2 = cnt[2][2], t2 = cnt[0][3], r2 = a2 + g2, y2 = c2 + t2;
+	
+	const score_t aagg = (g0 * (g0 - 1) * a1 * a2 + g1 * (g1 - 1) * a2 * a0 + g2 * (g2 - 1) * a0 * a1) * 2 * Y * Y;
+	const score_t aayy = (y0 * (y0 - 1) * a1 * a2 + y1 * (y1 - 1) * a2 * a0 + y2 * (y2 - 1) * a0 * a1) * G * R;
+	const score_t ggyy = (y0 * (y0 - 1) * g1 * g2 + y1 * (y1 - 1) * g2 * g0 + y2 * (y2 - 1) * g0 * g1) * A * R;
+	const score_t yyag = (a0 * g0 * y1 * y2 + a1 * g1 * y2 * y0 + a2 * g2 * y0 * y1) * R * R;
+	const score_t aagy = (g0 * y0 * a1 * a2 + g1 * y1 * a2 * a0 + g2 * y2 * a0 * a1) * 2 * R * Y;
+	const score_t ggay = (a0 * y0 * g1 * g2 + a1 * y1 * g2 * g0 + a2 * y2 * g0 * g1) * 2 * R * Y;
+	
+	const score_t cctt = (t0 * (t0 - 1) * c1 * c2 + t1 * (t1 - 1) * c2 * c0 + t2 * (t2 - 1) * c0 * c1) * 2 * R * R;
+	const score_t ccrr = (r0 * (r0 - 1) * c1 * c2 + r1 * (r1 - 1) * c2 * c0 + r2 * (r2 - 1) * c0 * c1) * T * Y;
+	const score_t ttrr = (r0 * (r0 - 1) * t1 * t2 + r1 * (r1 - 1) * t2 * t0 + r2 * (r2 - 1) * t0 * t1) * C * Y;
+	const score_t rrct = (c0 * t0 * r1 * r2 + c1 * t1 * r2 * r0 + c2 * t2 * r0 * r1) * Y * Y;
+	const score_t cctr = (t0 * r0 * c1 * c2 + t1 * r1 * c2 * c0 + t2 * r2 * c0 * c1) * 2 * R * Y;
+	const score_t ttcr = (c0 * r0 * t1 * t2 + c1 * r1 * t2 * t0 + c2 * r2 * t0 * t1) * 2 * R * Y;
+	
+	return aagg + aayy + ggyy - yyag - aagy - ggay + cctt + ccrr + ttrr - rrct - cctr - ttcr; 
 }
 
 struct TripartitionInitializer{
