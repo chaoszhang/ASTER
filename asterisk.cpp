9e0b6cc4c061ac2058cce11898ef6bef965bf357
Chao Zhang <chz069@ucsd.edu>
2020-10-17 02:59:42 -0700

    Add files via upload

--- asterisk.cpp
+++ asterisk.cpp
@@ -21,7 +21,6 @@ using namespace std;
 
 TripartitionInitializer tripInit;
 
-int npos = 0;
 vector<string> names;
 unordered_map<string, int> name2id;
 
@@ -34,83 +33,50 @@ string formatName(const string name){
 }
 
 void readFile(istream &fin){
-	int cnt[4] = {}, id = -1, oldNpos = npos;
+	tripInit.seq.emplace_back(names.size());
+	int cnt[2] = {}, id = -1, npos = 0;
 	string line;
 	while (getline(fin, line)){
 		if (line[0] == '>') {
-			if (id != -1 && npos < tripInit.seq[id].size()) npos = tripInit.seq[id].size();
 			id = name2id[formatName(line)];
 		}
 		else{
-			for (int i = 0; i + 1 < line.size(); i += 2){
-				const char c1 = line[i], c2 = line[i + 1];
-				const bool R1 = (c1 == 'A' || c1 == 'a' || c1 == 'G' || c1 == 'g');
-				const bool R2 = (c2 == 'A' || c2 == 'a' || c2 == 'G' || c2 == 'g');
-				const bool Y1 = (c1 == 'C' || c1 == 'c' || c1 == 'T' || c1 == 't');
-				const bool Y2 = (c2 == 'C' || c2 == 'c' || c2 == 'T' || c2 == 't');
-				const bool N1 = R1 || Y1 || (c1 == 'X' || c1 == 'x' || c1 == 'N' || c1 == 'n' || c1 == '-');
-				const bool N2 = R2 || Y2 || (c2 == 'X' || c2 == 'x' || c2 == 'N' || c2 == 'n' || c2 == '-');
-				if (R1 && R2){ tripInit.seq[id].push_back(0); cnt[0]++; }
-				else if (R1 && Y2){ tripInit.seq[id].push_back(1); cnt[1]++; }
-				else if (Y1 && R2){ tripInit.seq[id].push_back(2); cnt[2]++; }
-				else if (Y1 && Y2){ tripInit.seq[id].push_back(3); cnt[3]++; }
-				else if (N1 && N2){ tripInit.seq[id].push_back(-1); }
+			for (char c: line){
+				if (c == 'A' || c == 'a' || c == 'G' || c == 'g') { tripInit.seq.back()[id].push_back(0); cnt[0]++; }
+				if (c == 'C' || c == 'c' || c == 'T' || c == 't') { tripInit.seq.back()[id].push_back(1); cnt[1]++; }
+				if (c == 'X' || c == 'x' || c == 'N' || c == 'n' || c == '-') { tripInit.seq.back()[id].push_back(-1); }
 			}
-			if (npos < tripInit.seq[id].size()) npos = tripInit.seq[id].size();
+			if (npos < tripInit.seq.back()[id].size()) npos = tripInit.seq.back()[id].size();
 		}
 	}
-	for (id = 0; id < tripInit.seq.size(); id++){
-		while (tripInit.seq[id].size() < npos) tripInit.seq[id].push_back(-1);
+	for (id = 0; id < names.size(); id++){
+		while (tripInit.seq.back()[id].size() < npos) tripInit.seq.back()[id].push_back(-1);
 	}
-	score_t cntsum = cnt[0] + cnt[1] + cnt[2] + cnt[3];
-	while (tripInit.pi.size() < npos) tripInit.pi.push_back((cnt[0] * 2 + cnt[1] + cnt[2]) / (2 * cntsum));
-	for (int p = oldNpos; p < npos; p++){
-		int pcnt[4] = {};
-		for (id = 0; id < tripInit.seq.size(); id++){
-			if (tripInit.seq[id][p] != -1) pcnt[tripInit.seq[id][p]]++;
-		}
-		tripInit.weight.push_back(pcnt[0] + pcnt[1] + pcnt[2] + pcnt[3] - max({pcnt[0], pcnt[1], pcnt[2], pcnt[3]}) > 1);
-	}
+	tripInit.pi.push_back(cnt[0] / ((score_t) cnt[0] + cnt[1]));
 }
 
 void readPhilip(istream &fin){
 	string line;
 	int nTaxa, L;
 	while (fin >> nTaxa){
-		int cnt[4] = {}, oldNpos = npos;
+		tripInit.seq.emplace_back(names.size());
+		int cnt[4] = {}, npos = 0;
 		fin >> L;
 		for (int i = 0; i < nTaxa; i++){
 			fin >> line;
 			int id = name2id[line];
 			getline(fin, line);
-			for (int i = 0; i + 1 < line.size(); i++){
-				const char c1 = line[i], c2 = line[i + 1];
-				const bool R1 = (c1 == 'A' || c1 == 'a' || c1 == 'G' || c1 == 'g');
-				const bool R2 = (c2 == 'A' || c2 == 'a' || c2 == 'G' || c2 == 'g');
-				const bool Y1 = (c1 == 'C' || c1 == 'c' || c1 == 'T' || c1 == 't');
-				const bool Y2 = (c2 == 'C' || c2 == 'c' || c2 == 'T' || c2 == 't');
-				const bool N1 = R1 || Y1 || (c1 == 'X' || c1 == 'x' || c1 == 'N' || c1 == 'n' || c1 == '-');
-				const bool N2 = R2 || Y2 || (c2 == 'X' || c2 == 'x' || c2 == 'N' || c2 == 'n' || c2 == '-');
-				if (R1 && R2){ tripInit.seq[id].push_back(0); cnt[0]++; }
-				else if (R1 && Y2){ tripInit.seq[id].push_back(1); cnt[1]++; }
-				else if (Y1 && R2){ tripInit.seq[id].push_back(2); cnt[2]++; }
-				else if (Y1 && Y2){ tripInit.seq[id].push_back(3); cnt[3]++; }
-				else if (N1 && N2){ tripInit.seq[id].push_back(-1); }
+			for (char c: line){
+				if (c == 'A' || c == 'a' || c == 'G' || c == 'g') { tripInit.seq.back()[id].push_back(0); cnt[0]++; }
+				if (c == 'C' || c == 'c' || c == 'T' || c == 't') { tripInit.seq.back()[id].push_back(1); cnt[1]++; }
+				if (c == 'X' || c == 'x' || c == 'N' || c == 'n' || c == '-') { tripInit.seq.back()[id].push_back(-1); }
 			}
-			if (npos < tripInit.seq[id].size()) npos = tripInit.seq[id].size();
+			if (npos < tripInit.seq.back()[id].size()) npos = tripInit.seq.back()[id].size();
 		}
-		for (int id = 0; id < tripInit.seq.size(); id++){
-			while (tripInit.seq[id].size() < npos) tripInit.seq[id].push_back(-1);
+		for (int id = 0; id < names.size(); id++){
+			while (tripInit.seq.back()[id].size() < npos) tripInit.seq.back()[id].push_back(-1);
 		}
-		score_t cntsum = cnt[0] + cnt[1] + cnt[2] + cnt[3];
-		while (tripInit.pi.size() < npos) tripInit.pi.push_back((cnt[0] * 2 + cnt[1] + cnt[2]) / (2 * cntsum));
-		for (int p = oldNpos; p < npos; p++){
-			int pcnt[4] = {};
-			for (int id = 0; id < tripInit.seq.size(); id++){
-				if (tripInit.seq[id][p] != -1) pcnt[tripInit.seq[id][p]]++;
-			}
-			tripInit.weight.push_back(pcnt[0] + pcnt[1] + pcnt[2] + pcnt[3] - max({pcnt[0], pcnt[1], pcnt[2], pcnt[3]}) > 1);
-		}
+		tripInit.pi.push_back(cnt[0] / ((score_t) cnt[0] + cnt[1]));
 	}
 }
 
@@ -166,7 +132,6 @@ int main(int argc, char** argv){
 					if(name2id.count(s) == 0) {
 						name2id[s] = names.size();
 						names.push_back(s);
-						tripInit.seq.push_back({});
 					}
 				}
 			}
@@ -191,7 +156,6 @@ int main(int argc, char** argv){
 				if(name2id.count(s) == 0) {
 					name2id[s] = names.size();
 					names.push_back(s);
-					tripInit.seq.push_back({});
 				}
 			}
 		}
@@ -201,14 +165,8 @@ int main(int argc, char** argv){
 		}
 	}
 	
-	for (int i = 0; i < names.size(); i++){
-		tripInit.weightHelper.push_back(0);
-	}
-	tripInit.weightHelper.push_back(0);
-	
-	tripInit.npos = npos;
 	cerr << "#Species: " << names.size() << endl;
-	cerr << "#Bases: " << npos << endl;
+	cerr << "#Genes: " << tripInit.pi.size() << endl;
 	cerr << "#Rounds: " << nRounds << endl;
 	cerr << "#Samples: " << nSample << endl;
 	cerr << "#Threads: " << nThreads << "x" << tripInit.nThreads << endl;
--- sequencenontrivialpi.hpp
+++ sequencenontrivialpi.hpp
@@ -4,135 +4,143 @@
 
 using namespace std;
 
-inline score_t scorePos(const array<array<int, 4>, 3> cnt, const score_t pi){
-	const score_t R = pi, Y = 1 - R;
-	const long long r0 = cnt[0][0], p0 = cnt[0][1], q0 = cnt[0][2], y0 = cnt[0][3], n0 = r0 + p0 + q0 + y0;
-	const long long r1 = cnt[1][0], p1 = cnt[1][1], q1 = cnt[1][2], y1 = cnt[1][3], n1 = r1 + p1 + q1 + y1;
-	const long long r2 = cnt[2][0], p2 = cnt[2][1], q2 = cnt[2][2], y2 = cnt[2][3], n2 = r2 + p2 + q2 + y2;
-	
-	const score_t rrnn = r0 * (r0 - 1) * n1 * n2 + r1 * (r1 - 1) * n2 * n0 + r2 * (r2 - 1) * n0 * n1;
-	const score_t yynn = y0 * (y0 - 1) * n1 * n2 + y1 * (y1 - 1) * n2 * n0 + y2 * (y2 - 1) * n0 * n1;
-	const score_t rryy = r0 * (r0 - 1) * y1 * y2 + r1 * (r1 - 1) * y2 * y0 + r2 * (r2 - 1) * y0 * y1;
-	const score_t ppqq = p0 * (p0 - 1) * q1 * q2 + p1 * (p1 - 1) * q2 * q0 + p2 * (p2 - 1) * q0 * q1;
-	
-	const score_t rrpq = p0 * q0 * r1 * r2 + p1 * q1 * r2 * r0 + p2 * q2 * r0 * r1;
-	const score_t yypq = p0 * q0 * y1 * y2 + p1 * q1 * y2 * y0 + p2 * q2 * y0 * y1;
-	
-	return ((R > Y) ? 1 : -1) * (R*R * yynn - Y*Y * rrnn + (R - Y)*(2*rryy - ppqq) + (Y*Y - R*R)*(rryy + rrpq + yypq));
+score_t scoreGene(const vector<array<array<int, 2>, 3> > &cnt, const score_t R){
+	long long rryy0 = 0, rrnn0 = 0, nnyy0 = 0, nnnn0 = 0;
+	long long rryy1 = 0, rrnn1 = 0, nnyy1 = 0, nnnn1 = 0;
+	long long rryy2 = 0, rrnn2 = 0, nnyy2 = 0, nnnn2 = 0;
+	for (const array<array<int, 2>, 3> &c: cnt){
+		const long long r0 = c[0][0], y0 = c[0][1], n0 = r0 + y0;
+		const long long r1 = c[1][0], y1 = c[1][1], n1 = r1 + y1;
+		const long long r2 = c[2][0], y2 = c[2][1], n2 = r2 + y2;
+		rryy0 += r0 * (r0 - 1) * y1 * y2; rrnn0 += r0 * (r0 - 1) * n1 * n2; nnyy0 += n0 * (n0 - 1) * y1 * y2; nnnn0 += n0 * (n0 - 1) * n1 * n2;
+		rryy1 += r1 * (r1 - 1) * y2 * y0; rrnn1 += r1 * (r1 - 1) * n2 * n0; nnyy1 += n1 * (n1 - 1) * y2 * y0; nnnn1 += n1 * (n1 - 1) * n2 * n0;
+		rryy2 += r2 * (r2 - 1) * y0 * y1; rrnn2 += r2 * (r2 - 1) * n0 * n1; nnyy2 += n2 * (n2 - 1) * y0 * y1; nnnn2 += n2 * (n2 - 1) * n0 * n1;
+	}
+	return (rryy0 + rryy1 + rryy2) - (1 - 2 * R + 2 * R * R) * (
+			  ((nnnn0 != 0) ? ((score_t) rrnn0) * ((score_t) nnyy0) / ((score_t) nnnn0) : 0)
+			+ ((nnnn1 != 0) ? ((score_t) rrnn1) * ((score_t) nnyy1) / ((score_t) nnnn1) : 0)
+			+ ((nnnn2 != 0) ? ((score_t) rrnn2) * ((score_t) nnyy2) / ((score_t) nnnn2) : 0));
 }
 
 struct TripartitionInitializer{
-	int npos, nThreads = 1;
+	int nThreads = 1;
 	vector<score_t> pi;
-	vector<vector<int> > seq;
-	vector<bool> weight;
-	vector<score_t> weightHelper;
+	vector<vector<vector<int> > > seq;
 };
 
 struct Tripartition{
 	int nThreads, n = 0;
-	const int npos;
-	vector<array<array<int, 4>, 3> > cnt; // pos * part * letter
-	vector<array<int, 4> > total; // pos * letter
-	const vector<score_t> pi; // pos * letter
-	const vector<vector<int> > seq; // taxon * pos -> letter
-	const vector<bool> weight;
-	const vector<score_t> weightHelper;
-	// i, j, k -> taxon; p, q, r -> pos; x, y, z -> part; a, b, c -> letter
+	vector<vector<array<array<int, 2>, 3> > > cnt; // gene * pos * part * letter
+	vector<vector<array<int, 2> > > total; // gene * pos * letter
+	const vector<score_t> pi; // gene
+	const vector<vector<vector<int> > > seq; // gene * taxon * pos -> letter
+	// i, j, k -> taxon; p, q, r -> pos; x, y, z -> part; a, b, c -> letter; g -> gene
 	
-	Tripartition(const TripartitionInitializer &init): nThreads(init.nThreads), npos(init.npos), seq(init.seq), pi(init.pi), weight(init.weight), cnt(init.npos), total(init.npos), weightHelper(init.weightHelper){}
+	Tripartition(const TripartitionInitializer &init): nThreads(init.nThreads), seq(init.seq), pi(init.pi){
+		for (const vector<vector<int> > s: seq){
+			cnt.emplace_back(s[0].size());
+			total.emplace_back(s[0].size());
+		}
+	}
 	
 	void resetWork(int start, int end){
-		for (int p = start; p < end; p++){
-			for (int a = 0; a < 4; a++){
-				cnt[p][0][a] = total[p][a];
-				cnt[p][1][a] = 0;
-				cnt[p][2][a] = 0;
+		for (int g = start; g < end; g++){
+			for (int p = 0; p < total[g].size(); p++){
+				for (int a = 0; a < 2; a++){
+					cnt[g][p][0][a] = total[g][p][a];
+					cnt[g][p][1][a] = 0;
+					cnt[g][p][2][a] = 0;
+				}
 			}
 		}
 	}
 	
 	void addTotalWork(int i, int start, int end){
-		for (int p = start; p < end; p++){
-			int a = seq[i][p];
-			if (a != -1) total[p][a]++;
+		for (int g = start; g < end; g++){
+			for (int p = 0; p < total[g].size(); p++){
+				int a = seq[g][i][p];
+				if (a != -1) total[g][p][a]++;
+			}
 		}
 	}
 	
 	void rmvTotalWork(int i, int start, int end){
-		for (int p = start; p < end; p++){
-			int a = seq[i][p];
-			if (a != -1) total[p][a]--;
+		for (int g = start; g < end; g++){
+			for (int p = 0; p < total[g].size(); p++){
+				int a = seq[g][i][p];
+				if (a != -1) total[g][p][a]--;
+			}
 		}
 	}
 	
 	void addWork(int x, int i, int start, int end){
-		for (int p = start; p < end; p++){
-			int a = seq[i][p];
-			if (a != -1) cnt[p][x][a]++;
+		for (int g = start; g < end; g++){
+			for (int p = 0; p < total[g].size(); p++){
+				int a = seq[g][i][p];
+				if (a != -1) cnt[g][p][x][a]++;
+			}
 		}
 	}
 	
 	void rmvWork(int x, int i, int start, int end){
-		for (int p = start; p < end; p++){
-			int a = seq[i][p];
-			if (a != -1) cnt[p][x][a]--;
+		for (int g = start; g < end; g++){
+			for (int p = 0; p < total[g].size(); p++){
+				int a = seq[g][i][p];
+				if (a != -1) cnt[g][p][x][a]--;
+			}
 		}
 	}
 	
 	void scoreWork(score_t &res, int start, int end){
-		score_t temp = 0, tempH = 0;
-		for (int p = start; p < end; p++){
-			if (weight[p]) {
-				temp += scorePos(cnt[p], pi[p]);
-				//tempH += scorePosHelper(cnt[p], pi[p]);
-			}
+		score_t temp = 0;
+		for (int g = start; g < end; g++){
+			temp += scoreGene(cnt[g], pi[g]);
 		}
-		res = temp; // + tempH * weightHelper[n];
+		res = temp;
 	}
 	
 	void reset(){
 		vector<thread> thrds;
-		for (int t = 1; t < nThreads; t++) thrds.emplace_back(&Tripartition::resetWork, this, npos * t / nThreads, npos * (t + 1) / nThreads);
-		resetWork(0, npos / nThreads);
+		for (int t = 1; t < nThreads; t++) thrds.emplace_back(&Tripartition::resetWork, this, pi.size() * t / nThreads, pi.size() * (t + 1) / nThreads);
+		resetWork(0, pi.size() / nThreads);
 		for (thread &t: thrds) t.join();
 	}
 	
 	void addTotal(int i){
 		n++;
 		vector<thread> thrds;
-		for (int t = 1; t < nThreads; t++) thrds.emplace_back(&Tripartition::addTotalWork, this, i, npos * t / nThreads, npos * (t + 1) / nThreads);
-		addTotalWork(i, 0, npos / nThreads);
+		for (int t = 1; t < nThreads; t++) thrds.emplace_back(&Tripartition::addTotalWork, this, i, pi.size() * t / nThreads, pi.size() * (t + 1) / nThreads);
+		addTotalWork(i, 0, pi.size() / nThreads);
 		for (thread &t: thrds) t.join();
 	}
 	
 	void rmvTotal(int i){
 		n--;
 		vector<thread> thrds;
-		for (int t = 1; t < nThreads; t++) thrds.emplace_back(&Tripartition::rmvTotalWork, this, i, npos * t / nThreads, npos * (t + 1) / nThreads);
-		rmvTotalWork(i, 0, npos / nThreads);
+		for (int t = 1; t < nThreads; t++) thrds.emplace_back(&Tripartition::rmvTotalWork, this, i, pi.size() * t / nThreads, pi.size() * (t + 1) / nThreads);
+		rmvTotalWork(i, 0, pi.size() / nThreads);
 		for (thread &t: thrds) t.join();
 	}
 	
 	void add(int x, int i){
 		vector<thread> thrds;
-		for (int t = 1; t < nThreads; t++) thrds.emplace_back(&Tripartition::addWork, this, x, i, npos * t / nThreads, npos * (t + 1) / nThreads);
-		addWork(x, i, 0, npos / nThreads);
+		for (int t = 1; t < nThreads; t++) thrds.emplace_back(&Tripartition::addWork, this, x, i, pi.size() * t / nThreads, pi.size() * (t + 1) / nThreads);
+		addWork(x, i, 0, pi.size() / nThreads);
 		for (thread &t: thrds) t.join();
 	}
 	
 	void rmv(int x, int i){
 		vector<thread> thrds;
-		for (int t = 1; t < nThreads; t++) thrds.emplace_back(&Tripartition::rmvWork, this, x, i, npos * t / nThreads, npos * (t + 1) / nThreads);
-		rmvWork(x, i, 0, npos / nThreads);
+		for (int t = 1; t < nThreads; t++) thrds.emplace_back(&Tripartition::rmvWork, this, x, i, pi.size() * t / nThreads, pi.size() * (t + 1) / nThreads);
+		rmvWork(x, i, 0, pi.size() / nThreads);
 		for (thread &t: thrds) t.join();
 	}
 	
 	score_t score(){
 		vector<score_t> res(nThreads);
 		vector<thread> thrds;
-		for (int t = 1; t < nThreads; t++) thrds.emplace_back(&Tripartition::scoreWork, this, ref(res[t]), npos * t / nThreads, npos * (t + 1) / nThreads);
-		scoreWork(res[0], 0, npos / nThreads);
+		for (int t = 1; t < nThreads; t++) thrds.emplace_back(&Tripartition::scoreWork, this, ref(res[t]), pi.size() * t / nThreads, pi.size() * (t + 1) / nThreads);
+		scoreWork(res[0], 0, pi.size() / nThreads);
 		for (thread &t: thrds) t.join();
 		for (int t = 1; t < nThreads; t++) res[0] += res[t];
 		return res[0];
