25de548487acfe8c93a16ca00487749c8a1cad4e
Chao Zhang <chz069@ucsd.edu>
2021-01-14 14:31:03 -0800

    Add files via upload

--- astral-lengthweighted.cpp
+++ astral-lengthweighted.cpp
@@ -35,14 +35,17 @@ score_t from_string(const string s){
 #include "algorithms.hpp"
 
 
-TripartitionInitializer tripInit;
+MetaAlgorithm meta;
+TripartitionInitializer &tripInit = meta.tripInit;
+vector<TripartitionInitializer> &batchInit = meta.batchInit;
 
 unordered_map<string, string> leafname_mapping;
 string TEXT;
 int pos = 0;
 int K = 0;
-vector<string> names;
-unordered_map<string, int> name2id;
+int part = 0, iBatch = 0;
+vector<string> &names = meta.names;
+unordered_map<string, int> &name2id = meta.name2id;
 
 
 int MAPPING(int begin, int end){
@@ -54,7 +57,7 @@ int MAPPING(int begin, int end){
 	if (name2id.count(s) == 0){
 		name2id[s] = names.size();
 		names.push_back(s);
-		tripInit.leafParent.emplace_back();
+		tripInit.leafParent[part].emplace_back();
 	}
 	return name2id[s];
 }
@@ -67,38 +70,38 @@ score_t WEIGHT(int begin, int end){
 }
 
 void parse(int parent = -1, bool isLeft = true){
-	int cur = tripInit.nodes.size();
-	tripInit.nodes.emplace_back();
-	tripInit.nodes[cur].up = parent;
-	if (parent != -1 && isLeft) tripInit.nodes[parent].small = cur;
-	if (parent != -1 && !isLeft) tripInit.nodes[parent].large = cur;
+	int cur = tripInit.nodes[part].size();
+	tripInit.nodes[part].emplace_back();
+	tripInit.nodes[part][cur].up = parent;
+	if (parent != -1 && isLeft) tripInit.nodes[part][parent].small = cur;
+	if (parent != -1 && !isLeft) tripInit.nodes[part][parent].large = cur;
 	
-	if (TEXT[pos] == '(') { //cerr << TEXT[pos];
+	if (TEXT[pos] == '(') { 
 		pos++;
-		parse(cur, true); //cerr << TEXT[pos];
+		parse(cur, true); 
 		pos++;
-		parse(cur, false); //cerr << TEXT[pos];
+		parse(cur, false); 
 		while (TEXT[pos] != ')'){
-			int next = tripInit.nodes.size();
-			tripInit.nodes.emplace_back();
-			tripInit.nodes[cur].up = next;
-			tripInit.nodes[next].small = cur;
+			int next = tripInit.nodes[part].size();
+			tripInit.nodes[part].emplace_back();
+			tripInit.nodes[part][cur].up = next;
+			tripInit.nodes[part][next].small = cur;
 			cur = next;
-			tripInit.nodes[cur].up = parent;
-			if (parent != -1 && isLeft) tripInit.nodes[parent].small = cur;
-			if (parent != -1 && !isLeft) tripInit.nodes[parent].large = cur;
+			tripInit.nodes[part][cur].up = parent;
+			if (parent != -1 && isLeft) tripInit.nodes[part][parent].small = cur;
+			if (parent != -1 && !isLeft) tripInit.nodes[part][parent].large = cur;
 			pos++;
-			parse(cur, false); //cerr << TEXT[pos];
+			parse(cur, false);
 		}
 		int i = ++pos;
 		while (TEXT[pos] != ')' && TEXT[pos] != ',' && TEXT[pos] != ';') pos++;
-		tripInit.nodes[cur].weight = WEIGHT(i, pos); //cerr << ":" << tripInit.nodes[cur].weight;
+		tripInit.nodes[part][cur].weight = WEIGHT(i, pos);
 	} 
 	else {
 		int i = pos;
 		while (TEXT[pos] != ')' && TEXT[pos] != ',') pos++;
-		tripInit.leafParent[MAPPING(i, pos)].push_back(cur);
-		tripInit.nodes[cur].weight = WEIGHT(i, pos); //cerr << "'"<< names[MAPPING(i, pos)] << "':" << tripInit.nodes[cur].weight;
+		tripInit.leafParent[part][MAPPING(i, pos)].push_back(cur);
+		tripInit.nodes[part][cur].weight = WEIGHT(i, pos);
 	}
 }
 
@@ -117,67 +120,38 @@ void readInputTrees(string input, string mapping) {
 	while (pos < TEXT.size()){
 		while (pos < TEXT.size() && TEXT[pos] != '(') pos++;
 		if (pos < TEXT.size()) {
-			parse(); //cerr << endl;
+			part = K % tripInit.nodes.size();
+			parse();
 			K++;
 		}
 	}
 }
 
-string helpText = R"V0G0N(astral_feast [-o oFilePath -r nRound -s nSample -p probability -t nThread -a taxonNameMaps] inputGeneTrees
--o  path to output file (default: stdout)
--r  number of total rounds of placements (default: 5)
--s  number of total rounds of subsampling (default: 0)
--p  subsampling probability of keeping each taxon (default: 0.5)
--t  number of threads (default: 1)
--a  a list of gene name to taxon name maps, each line contains one gene name followed by one taxon name separated by a space or tab 
+string HELP_TEXT = R"V0G0N(-a  a list of gene name to taxon name maps, each line contains one gene name followed by one taxon name separated by a space or tab 
 inputGeneTrees: the path to a file containing all gene trees in Newick format
 )V0G0N";
 
 int main(int argc, char** argv){
-	int nThreads = 1, nRounds = 4, nSample = 0;
-	bool phylip = false;
-	double p = 0.5;
-	string outputFile, mappingFile;
-	ofstream fileOut;
-	if (argc == 1) {cerr << helpText; return 0;}
+	string mappingFile;
+	meta.initialize(argc, argv, " -a taxonNameMaps", HELP_TEXT);
+	
 	for (int i = 1; i < argc; i += 2){
 		if (strcmp(argv[i], "-a") == 0) mappingFile = argv[i + 1];
-		
-		if (strcmp(argv[i], "-o") == 0) outputFile = argv[i + 1];
-		if (strcmp(argv[i], "-r") == 0) sscanf(argv[i + 1], "%d", &nRounds);
-		if (strcmp(argv[i], "-s") == 0) sscanf(argv[i + 1], "%d", &nSample);
-		if (strcmp(argv[i], "-p") == 0) sscanf(argv[i + 1], "%lf", &p);
-		if (strcmp(argv[i], "-t") == 0) sscanf(argv[i + 1], "%d", &nThreads);
-		if (strcmp(argv[i], "-h") == 0) {cerr << helpText; return 0;}
 	}
-	ostream &fout = (outputFile == "") ? cout : fileOut;
-	if (outputFile != "") fileOut.open(outputFile);
+	
+	for (int i = 0; i < meta.nThread2; i++){
+		tripInit.nodes.emplace_back();
+		tripInit.leafParent.emplace_back();
+	}
+	for (int i = 0; i < meta.nBatch; i++){
+		batchInit[i].nodes.emplace_back();
+		batchInit[i].leafParent.emplace_back();
+	}
 	readInputTrees(argv[argc - 1], mappingFile);
 	
-	/*
-	for (auto e: tripInit.parentChild[0]){
-		cerr << e.first << " ";
-		if (e.second < tripInit.nTaxa) cerr << names[e.second] << endl; else cerr << e.second << endl;
-	}
-	*/
-	
-	
-	cerr << "#Species: " << names.size() << endl;
 	cerr << "#Genetrees: " << K << endl;
-	cerr << "#Rounds: " << nRounds << endl;
-	cerr << "#Samples: " << nSample << endl;
-	cerr << "#Threads: " << nThreads << endl;
-	cerr << "p = " << p << endl;
 	
-	ConstrainedOptimizationAlgorithm alg(names.size(), tripInit, names);
-	auto res = alg.run(nRounds, nThreads);
-	cerr << "Score: " << res.first << endl;
-	cerr << res.second << endl;
-	
-	res = alg.run(nSample, nThreads, p);
-	cerr << "Score: " << res.first << endl;
-	fout << res.second << endl;
-	
-	//cerr << alg.printOptimalTreeWithScore() << endl;
+	score_t score = meta.run().first / 2;
+	cerr << "Score: " << score << endl;
 	return 0;
 }
--- genetreewithlength.hpp
+++ genetreewithlength.hpp
@@ -1,7 +1,6 @@
-typedef double score_t;
-
 #include<vector>
 #include<array>
+#include<thread>
 
 using namespace std;
 
@@ -11,140 +10,186 @@ struct TripartitionInitializer{
 		score_t weight = 1;
 	};
 	
-	vector<Node> nodes;
-	vector<vector<int> > leafParent;
+	vector<vector<Node> > nodes;
+	vector<vector<vector<int> > > leafParent;
 };
 
 struct Tripartition{
-	struct Node{
-		score_t x = 0, y = 0, z = 0, tx = 0, ty = 0, tz = 0, q = 0;
-		score_t x2a = 0, y2a = 0, z2a = 0, xya = 0, xza = 0, yza = 0;
-		score_t x2b = 0, y2b = 0, z2b = 0, xyb = 0, xzb = 0, yzb = 0;
-		
-		int version = 0, up = -1, small = -1, large = -1; // -1 for dummy!
-		score_t weight = 1;
-		
-		void update(int v, const Node &t){
-			if (version != v){
-				*this = t;
-				version = v;
+	struct Partition{
+		struct Node{
+			score_t x = 0, y = 0, z = 0, tx = 0, ty = 0, tz = 0, q = 0;
+			score_t x2a = 0, y2a = 0, z2a = 0, xya = 0, xza = 0, yza = 0;
+			score_t x2b = 0, y2b = 0, z2b = 0, xyb = 0, xzb = 0, yzb = 0;
+			
+			int version = 0, up = -1, small = -1, large = -1; // -1 for dummy!
+			score_t weight = 1;
+			
+			void update(int v, const Node &t){
+				if (version != v){
+					*this = t;
+					version = v;
+				}
 			}
+		};
+		
+		score_t normal(Node& w){
+			Node& u = nodes[w.small];
+			Node& v = nodes[w.large];
+			u.update(version, totalZ[w.small]);
+			v.update(version, totalZ[w.large]);
+			
+			w.x = (u.x + v.x) * w.weight;
+			w.y = (u.y + v.y) * w.weight;
+			w.z = (u.z + v.z) * w.weight;
+			w.x2a = u.x2a + v.x2a + u.x * v.x;
+			w.y2a = u.y2a + v.y2a + u.y * v.y;
+			w.z2a = u.z2a + v.z2a + u.z * v.z;
+			w.xya = u.xya + v.xya + u.x * v.y + u.y * v.x;
+			w.xza = u.xza + v.xza + u.x * v.z + u.z * v.x;
+			w.yza = u.yza + v.yza + u.y * v.z + u.z * v.y;
+			w.x2b = (u.x2b + v.x2b + u.x * v.x) * w.weight;
+			w.y2b = (u.y2b + v.y2b + u.y * v.y) * w.weight;
+			w.z2b = (u.z2b + v.z2b + u.z * v.z) * w.weight;
+			w.xyb = (u.xyb + v.xyb + u.x * v.y + u.y * v.x) * w.weight;
+			w.xzb = (u.xzb + v.xzb + u.x * v.z + u.z * v.x) * w.weight;
+			w.yzb = (u.yzb + v.yzb + u.y * v.z + u.z * v.y) * w.weight;
+			
+			
+			w.tx = (u.tx + v.tx + u.y * (v.z2a - v.z2b) + (u.z2a - u.z2b) * v.y + u.z * (v.y2a - v.y2b) + (u.y2a - u.y2b) * v.z) * w.weight;
+			w.ty = (u.ty + v.ty + u.x * (v.z2a - v.z2b) + (u.z2a - u.z2b) * v.x + u.z * (v.x2a - v.x2b) + (u.x2a - u.x2b) * v.z) * w.weight;
+			w.tz = (u.tz + v.tz + u.x * (v.y2a - v.y2b) + (u.y2a - u.y2b) * v.x + u.y * (v.x2a - v.x2b) + (u.x2a - u.x2b) * v.y) * w.weight;
+			score_t oldQ = w.q;
+			w.q = u.x * v.tx + u.y * v.ty + u.z * v.tz + v.x * u.tx + v.y * u.ty + v.z * u.tz
+				+ u.x2a * v.yza - u.x2b * v.yzb
+				+ u.y2a * v.xza - u.y2b * v.xzb
+				+ u.z2a * v.xya - u.z2b * v.xyb;
+			return w.q - oldQ;
+		}
+		
+		vector<vector<int> > leafParent;
+		score_t totalScore = 0;
+		vector<Node> nodes, totalZ;
+		int version = 0;
+		
+		Partition(TripartitionInitializer init, int p): leafParent(init.leafParent[p]), nodes(init.nodes[p].size()), totalZ(init.nodes[p].size()){
+			for (int i = 0; i < nodes.size(); i++){
+				nodes[i].up = init.nodes[p][i].up;
+				nodes[i].small = init.nodes[p][i].small;
+				nodes[i].large = init.nodes[p][i].large;
+				nodes[i].weight = init.nodes[p][i].weight;
+				totalZ[i] = nodes[i];
+			}
+		}
+		
+		void reset(){
+			totalScore = 0;
+			version++;
+		}
+		
+		void addTotal(int i){
+			for (int w: leafParent[i]){
+				totalZ[w].z += totalZ[w].weight;
+				w = totalZ[w].up;
+				while (w != -1){
+					Node& u = totalZ[totalZ[w].small];
+					Node& v = totalZ[totalZ[w].large];
+					totalZ[w].z = (u.z + v.z) * totalZ[w].weight;
+					totalZ[w].z2a = u.z2a + v.z2a + u.z * v.z;
+					totalZ[w].z2b = (u.z2b + v.z2b + u.z * v.z) * totalZ[w].weight;
+					w = totalZ[w].up;
+				}
+			}
+		}
+		
+		void rmvTotal(int i){
+			for (int w: leafParent[i]){
+				totalZ[w].z -= totalZ[w].weight;
+				w = totalZ[w].up;
+				while (w != -1){
+					Node& u = totalZ[totalZ[w].small];
+					Node& v = totalZ[totalZ[w].large];
+					totalZ[w].z = (u.z + v.z) * totalZ[w].weight;
+					totalZ[w].z2a = u.z2a + v.z2a + u.z * v.z;
+					totalZ[w].z2b = (u.z2b + v.z2b + u.z * v.z) * totalZ[w].weight;
+					w = totalZ[w].up;
+				}
+			}
+		}
+		
+		void add(int x, int i){
+			for (int u: leafParent[i]){
+				nodes[u].update(version, totalZ[u]);
+				if (x == 0) nodes[u].z += nodes[u].weight; else if (x == 1) nodes[u].x += nodes[u].weight; else nodes[u].y += nodes[u].weight;
+				int w = nodes[u].up;
+				while (w != -1){
+					nodes[w].update(version, totalZ[w]);
+					totalScore += normal(nodes[w]);
+					u = w;
+					w = nodes[u].up;
+				}
+			}
+		}
+		
+		void rmv(int x, int i){
+			for (int u: leafParent[i]){
+				nodes[u].update(version, totalZ[u]);
+				if (x == 0) nodes[u].z -= nodes[u].weight; else if (x == 1) nodes[u].x -= nodes[u].weight; else nodes[u].y -= nodes[u].weight;
+				int w = nodes[u].up;
+				while (w != -1){
+					nodes[w].update(version, totalZ[w]);
+					totalScore += normal(nodes[w]);
+					u = w;
+					w = nodes[u].up;
+				}
+			}
+		}
+		
+		score_t score(){
+			return totalScore;
 		}
 	};
 	
-	score_t normal(Node& w){
-		Node& u = nodes[w.small];
-		Node& v = nodes[w.large];
-		u.update(version, totalZ[w.small]);
-		v.update(version, totalZ[w.large]);
-		
-		w.x = (u.x + v.x) * w.weight;
-		w.y = (u.y + v.y) * w.weight;
-		w.z = (u.z + v.z) * w.weight;
-		w.x2a = u.x2a + v.x2a + u.x * v.x;
-		w.y2a = u.y2a + v.y2a + u.y * v.y;
-		w.z2a = u.z2a + v.z2a + u.z * v.z;
-		w.xya = u.xya + v.xya + u.x * v.y + u.y * v.x;
-		w.xza = u.xza + v.xza + u.x * v.z + u.z * v.x;
-		w.yza = u.yza + v.yza + u.y * v.z + u.z * v.y;
-		w.x2b = (u.x2b + v.x2b + u.x * v.x) * w.weight;
-		w.y2b = (u.y2b + v.y2b + u.y * v.y) * w.weight;
-		w.z2b = (u.z2b + v.z2b + u.z * v.z) * w.weight;
-		w.xyb = (u.xyb + v.xyb + u.x * v.y + u.y * v.x) * w.weight;
-		w.xzb = (u.xzb + v.xzb + u.x * v.z + u.z * v.x) * w.weight;
-		w.yzb = (u.yzb + v.yzb + u.y * v.z + u.z * v.y) * w.weight;
-		
-		
-		w.tx = (u.tx + v.tx + u.y * (v.z2a - v.z2b) + (u.z2a - u.z2b) * v.y + u.z * (v.y2a - v.y2b) + (u.y2a - u.y2b) * v.z) * w.weight;
-		w.ty = (u.ty + v.ty + u.x * (v.z2a - v.z2b) + (u.z2a - u.z2b) * v.x + u.z * (v.x2a - v.x2b) + (u.x2a - u.x2b) * v.z) * w.weight;
-		w.tz = (u.tz + v.tz + u.x * (v.y2a - v.y2b) + (u.y2a - u.y2b) * v.x + u.y * (v.x2a - v.x2b) + (u.x2a - u.x2b) * v.y) * w.weight;
-		score_t oldQ = w.q;
-		w.q = u.x * v.tx + u.y * v.ty + u.z * v.tz + v.x * u.tx + v.y * u.ty + v.z * u.tz
-			+ u.x2a * v.yza - u.x2b * v.yzb
-			+ u.y2a * v.xza - u.y2b * v.xzb
-			+ u.z2a * v.xya - u.z2b * v.xyb;
-		return w.q - oldQ;
-	}
+	vector<Partition> parts;
 	
-	vector<vector<int> > leafParent;
-	score_t totalScore = 0;
-	vector<Node> nodes, totalZ;
-	int version = 0;
-	
-	Tripartition(TripartitionInitializer init): leafParent(init.leafParent), nodes(init.nodes.size()), totalZ(init.nodes.size()){
-		for (int i = 0; i < nodes.size(); i++){
-			nodes[i].up = init.nodes[i].up;
-			nodes[i].small = init.nodes[i].small;
-			nodes[i].large = init.nodes[i].large;
-			nodes[i].weight = init.nodes[i].weight;
-			totalZ[i] = nodes[i];
-		}
+	Tripartition(const TripartitionInitializer &init){
+		for (int p = 0; p < init.nodes.size(); p++) parts.emplace_back(init, p);
 	}
 	
 	void reset(){
-		totalScore = 0;
-		version++;
+		for (int p = 0; p < parts.size(); p++) parts[p].reset();
 	}
 	
 	void addTotal(int i){
-		for (int w: leafParent[i]){
-			totalZ[w].z += totalZ[w].weight;
-			w = totalZ[w].up;
-			while (w != -1){
-				Node& u = totalZ[totalZ[w].small];
-				Node& v = totalZ[totalZ[w].large];
-				totalZ[w].z = (u.z + v.z) * totalZ[w].weight;
-				totalZ[w].z2a = u.z2a + v.z2a + u.z * v.z;
-				totalZ[w].z2b = (u.z2b + v.z2b + u.z * v.z) * totalZ[w].weight;
-				w = totalZ[w].up;
-			}
-		}
+		vector<thread> thrds;
+		for (int p = 1; p < parts.size(); p++) thrds.emplace_back(&Partition::addTotal, &parts[p], i);
+		parts[0].addTotal(i);
+		for (thread &t: thrds) t.join();
 	}
 	
 	void rmvTotal(int i){
-		for (int w: leafParent[i]){
-			totalZ[w].z -= totalZ[w].weight;
-			w = totalZ[w].up;
-			while (w != -1){
-				Node& u = totalZ[totalZ[w].small];
-				Node& v = totalZ[totalZ[w].large];
-				totalZ[w].z = (u.z + v.z) * totalZ[w].weight;
-				totalZ[w].z2a = u.z2a + v.z2a + u.z * v.z;
-				totalZ[w].z2b = (u.z2b + v.z2b + u.z * v.z) * totalZ[w].weight;
-				w = totalZ[w].up;
-			}
-		}
+		vector<thread> thrds;
+		for (int p = 1; p < parts.size(); p++) thrds.emplace_back(&Partition::rmvTotal, &parts[p], i);
+		parts[0].rmvTotal(i);
+		for (thread &t: thrds) t.join();
 	}
 	
 	void add(int x, int i){
-		for (int u: leafParent[i]){
-			nodes[u].update(version, totalZ[u]);
-			if (x == 0) nodes[u].z += nodes[u].weight; else if (x == 1) nodes[u].x += nodes[u].weight; else nodes[u].y += nodes[u].weight;
-			int w = nodes[u].up;
-			while (w != -1){
-				nodes[w].update(version, totalZ[w]);
-				totalScore += normal(nodes[w]);
-				u = w;
-				w = nodes[u].up;
-			}
-		}
+		vector<thread> thrds;
+		for (int p = 1; p < parts.size(); p++) thrds.emplace_back(&Partition::add, &parts[p], x, i);
+		parts[0].add(x, i);
+		for (thread &t: thrds) t.join();
 	}
 	
 	void rmv(int x, int i){
-		for (int u: leafParent[i]){
-			nodes[u].update(version, totalZ[u]);
-			if (x == 0) nodes[u].z -= nodes[u].weight; else if (x == 1) nodes[u].x -= nodes[u].weight; else nodes[u].y -= nodes[u].weight;
-			int w = nodes[u].up;
-			while (w != -1){
-				nodes[w].update(version, totalZ[w]);
-				totalScore += normal(nodes[w]);
-				u = w;
-				w = nodes[u].up;
-			}
-		}
+		vector<thread> thrds;
+		for (int p = 1; p < parts.size(); p++) thrds.emplace_back(&Partition::rmv, &parts[p], x, i);
+		parts[0].rmv(x, i);
+		for (thread &t: thrds) t.join();
 	}
 	
 	score_t score(){
-		return totalScore;
+		score_t res = 0;
+		for (int p = 0; p < parts.size(); p++) res += parts[p].score();
+		return res;
 	}
 };
