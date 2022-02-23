c73ef1db7fed84e53aa7b651cb1d2a7ce559c119
Chao Zhang <chz069@ucsd.edu>
2022-02-23 08:30:42 -0800

    Add files via upload

--- src/biallelic-cuda.cuh
+++ src/biallelic-cuda.cuh
@@ -2,6 +2,14 @@
 
 #define BATCH 32
 #define BATCH_TYPE unsigned
+#define MAX_TAPE_SIZE 4096
+
+#define THREAD_POOL
+#ifdef THREAD_POOL
+#define TAPE_SIZE MAX_TAPE_SIZE
+#else
+#define TAPE_SIZE 1
+#endif
 
 struct GroupBatch{
 	double p[4][BATCH] = {};
@@ -20,3 +28,8 @@ void myCudaMemcpyD2H(void *T, void *S, int size);
 void myCudaFree(void *p);
 void cudaScore(GroupIBatch *GI, GroupFBatch *GF, SiteBatch* S, double* BS, int kc, int b);
 void cudaUpdate(GroupIBatch *GI, SiteBatch *S, HotBatch *H, int n, int x, int y, int i, int kc, int b);
+
+void myCudaMemcpyH2C(void *S, int size);
+void cudaWork(GroupIBatch *GI, GroupFBatch *GF, SiteBatch* S, double* BS, HotBatch *H, int n, int kc, int b, int tapeSize);
+
+//void cudaWork(GroupIBatch *GI, GroupFBatch *GF, SiteBatch* S, double* BS, HotBatch *H, int n, int kc, int b, int tapeSize, short *TAPE);
