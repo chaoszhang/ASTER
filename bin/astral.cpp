48cc860885b14fe29763e06909f24350699790b9
Chao Zhang <chz069@ucsd.edu>
2022-09-04 04:32:25 -0700

    Add files via upload

---
+++ bin/astral.cpp
@@ -0,0 +1,158 @@
+#define DRIVER_VERSION "1"
+
+/* CHANGE LOG
+ * 1: Use genetreewithbinaryweight.hpp instead of genetree.hpp 
+ */
+
+#include<iostream>
+#include<fstream>
+#include<unordered_map>
+#include<cstdio>
+#include<cstdlib>
+#include<cstring>
+#include<string>
+
+using namespace std;
+
+//#define LARGE_DATA
+#ifdef LARGE_DATA
+typedef __int128 score_t;
+
+string to_string(const __int128 x){
+	return to_string((double) x);
+}
+
+ostream& operator<<(ostream& cout, __int128 x){
+	return cout << to_string(x);
+}
+#else
+typedef long long score_t;
+#endif
+
+#include "argparser.hpp"
+#include "genetreewithbinaryweight.hpp"
+#include "algorithms.hpp"
+
+MetaAlgorithm meta;
+TripartitionInitializer &tripInit = meta.tripInit;
+vector<TripartitionInitializer> &batchInit = meta.batchInit;
+
+unordered_map<string, string> leafname_mapping;
+string TEXT;
+int pos = 0;
+int K = 0;
+int part = 0, iBatch = 0;
+vector<string> &names = meta.names;
+unordered_map<string, int> &name2id = meta.name2id;
+
+int MAPPING(int begin, int end){
+	string s;
+	for (int i = begin; i < end && TEXT[i] != ':'; i++){
+		if (TEXT[i] != '\"' && TEXT[i] != '\'') s += TEXT[i];
+	}
+	if (leafname_mapping.count(s)) s = leafname_mapping[s];
+	if (name2id.count(s) == 0){
+		name2id[s] = names.size();
+		names.push_back(s);
+		for (int i = 0; i < meta.nThread2; i++){
+			tripInit.leafParent[i].emplace_back();
+		}
+	}
+	return name2id[s];
+}
+
+void parse(int parent = -1, bool isLeft = true){
+	int cur = tripInit.nodes[part].size();
+	tripInit.nodes[part].emplace_back();
+	tripInit.nodes[part][cur].up = parent;
+	if (parent != -1 && isLeft) tripInit.nodes[part][parent].small = cur;
+	if (parent != -1 && !isLeft) tripInit.nodes[part][parent].large = cur;
+	
+	if (TEXT[pos] == '(') { 
+		pos++;
+		parse(cur, true); 
+		pos++;
+		parse(cur, false);
+		vector<int> lst;
+		lst.push_back(cur);
+		tripInit.nodes[part][cur].isGhostBranch = true;
+		while (TEXT[pos] != ')'){
+			int left = lst[rand() % lst.size()];
+			int up = tripInit.nodes[part].size();
+			tripInit.nodes[part].emplace_back();
+			lst.push_back(up);
+			if (cur == left) cur = up;
+			tripInit.nodes[part][up].isGhostBranch = true;
+			int g = tripInit.nodes[part][left].up;
+			if (g != -1){
+				if (tripInit.nodes[part][g].small == left) tripInit.nodes[part][g].small = up;
+				else tripInit.nodes[part][g].large = up;
+			}
+			tripInit.nodes[part][up].up = g;
+			tripInit.nodes[part][left].up = up; 
+			tripInit.nodes[part][up].small = left;
+			pos++;
+			parse(up, false);
+		}
+		int i = ++pos;
+		while (TEXT[pos] != ')' && TEXT[pos] != ',' && TEXT[pos] != ';') pos++;
+		tripInit.nodes[part][cur].isGhostBranch = false;
+	} 
+	else {
+		int i = pos;
+		while (TEXT[pos] != ')' && TEXT[pos] != ',') pos++;
+		tripInit.leafParent[part][MAPPING(i, pos)].push_back(cur);
+		tripInit.nodes[part][cur].isGhostBranch = false;
+	}
+}
+
+void readInputTrees(string input, string mapping) {
+	if (mapping != ""){
+		ifstream fmap(mapping);
+		string gname, sname;
+		while (fmap >> gname){
+			fmap >> sname;
+			leafname_mapping[gname] = sname;
+		}
+	}
+	ifstream fin(input);
+	string line;
+	while (getline(fin, line)) TEXT += line;
+	while (pos < TEXT.size()){
+		while (pos < TEXT.size() && TEXT[pos] != '(') pos++;
+		if (pos < TEXT.size()) {
+			part = K % tripInit.nodes.size();
+			parse();
+			K++;
+		}
+	}
+}
+
+string HELP_TEXT = R"V0G0N(-a  a list of gene name to taxon name maps, each line contains one gene name followed by one taxon name separated by a space or tab 
+inputGeneTrees: the path to a file containing all gene trees in Newick format
+)V0G0N";
+
+int main(int argc, char** argv){
+	ARG.setProgramName("astral", "Accurate Species TRee ALgorithm");
+	ARG.addStringArg('a', "mapping", "", "A list of gene name to taxon name maps, each line contains one gene name followed by one taxon name separated by a space or tab");
+	
+	string mappingFile;
+	meta.initialize(argc, argv, " -a taxonNameMaps", HELP_TEXT);
+	mappingFile = ARG.getStringArg("mapping");
+	
+	for (int i = 0; i < meta.nThread2; i++){
+		tripInit.nodes.emplace_back();
+		tripInit.leafParent.emplace_back();
+	}
+	for (int i = 0; i < meta.nBatch; i++){
+		batchInit[i].nodes.emplace_back();
+		batchInit[i].leafParent.emplace_back();
+	}
+	readInputTrees(ARG.getStringArg("input"), mappingFile);
+	
+	cerr << "#Genetrees: " << K << endl;
+	
+	score_t score = meta.run().first;
+	cerr << "Score: " << score << endl;
+	return 0;
+}
